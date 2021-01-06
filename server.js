4041662f74bd5a40fbf44e080104f6f744895007
unknown <csecec.1703176@gmail.com>
2021-01-06 18:48:10 +0530

    added cors

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const path = require('path');
+const cors = require('cors');
 
 const PORT=process.env.PORT || 3000;
 app.use(express.static('public'));
@@ -9,6 +10,14 @@ app.use(express.json());
 const connectDB = require('./config/db');
 
 connectDB();
+//cors
+const corsOptions={
+    origin: process.env.Allowed_Client.split(',')
+}
+app.use(cors(corsOptions));
+
+
+
 //template engines
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
