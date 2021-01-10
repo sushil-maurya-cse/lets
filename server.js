be957442fd6863e05d8b0bfa58e70b7c258db83f
sushil <65329897+sushilthelearner@users.noreply.github.com>
2021-01-10 14:04:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const connectDB = require('./config/db');
 connectDB();
 //cors
 const corsOptions={
-    origin: ['https://sushilthelearner.github.io','https://localhost:3000']
+    origin: ['https://sushilthelearner.github.io']
 }
 app.use(cors(corsOptions));
 
